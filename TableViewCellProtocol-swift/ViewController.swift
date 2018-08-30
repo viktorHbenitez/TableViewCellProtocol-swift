df5866396f807635f148935483a42d95286ac4d1
viktor hugo <victorHbenitez85@hotmail.com>
2018-08-29 21:11:14 -0500

    Create init project

---
+++ TableViewCellProtocol-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewCellProtocol-swift
+//
+//  Created by Victor Hugo Benitez Bosques on 29/08/18.
+//  Copyright © 2018 Victor Hugo Benitez Bosques. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ workflow-walkthrough.md
@@ -0,0 +1,3 @@
+# Make button Clickable in tableView Cell using Protocol
+
+
